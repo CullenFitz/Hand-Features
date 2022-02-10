c28b9c2eaf4d381512a0bc49e92344561a2db785
Cullen <cullenfitzgerald9@gmail.com>
2022-02-10 09:05:56 -0500

    Outline of hand is done

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import cv2
+import mediapipe
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+drawingModule = mediapipe.solutions.drawing_utils
+handsModule = mediapipe.solutions.hands
 
+with handsModule.Hands(static_image_mode=True) as hands:
+    image = cv2.imread(r'C:\Users\Cullen\Downloads\IMG_2555.jpg')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
+    imageHeight, imageWidth, _ = image.shape
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    if results.multi_hand_landmarks != None:
+        for handLandmarks in results.multi_hand_landmarks:
+            drawingModule.draw_landmarks(image, handLandmarks, handsModule.HAND_CONNECTIONS)
+            for point in handsModule.HandLandmark:
+                normalizedLandmark = handLandmarks.landmark[point]
+                pixelCoord = drawingModule._normalized_to_pixel_coordinates(normalizedLandmark.x,
+                                                                                          normalizedLandmark.y,
+                                                                                          imageWidth, imageHeight)
+                #cv2.line(image)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+                #print(point)
+                #print(pixelCoord)
+                #print(normalizedLandmark)
+
+    cv2.imshow('Hand', image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
