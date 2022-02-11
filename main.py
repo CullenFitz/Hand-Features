17f4418ee539df36bb119e949483c689323bafb3
Cullen <cullenfitzgerald9@gmail.com>
2022-02-11 10:56:24 -0500

    5 iterations

--- main.py
+++ main.py
@@ -2,36 +2,50 @@ import cv2
 import mediapipe
 import numpy as np
 
-drawingModule = mediapipe.solutions.drawing_utils
-handsModule = mediapipe.solutions.hands
+def HandFeatures(img):
 
-with handsModule.Hands(static_image_mode=True) as hands:
-    image = cv2.imread(r'C:\Users\Cullen\Downloads\IMG_2555.jpg')
+    drawingModule = mediapipe.solutions.drawing_utils
+    handsModule = mediapipe.solutions.hands
 
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    with handsModule.Hands(static_image_mode=True) as hands:
+        image = cv2.imread(img)
 
-    imageHeight, imageWidth, _ = image.shape
+        results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    if results.multi_hand_landmarks != None:
-        for handLandmarks in results.multi_hand_landmarks:
-            drawingModule.draw_landmarks(image, handLandmarks, handsModule.HAND_CONNECTIONS)
-            for point in handsModule.HandLandmark:
-                normalizedLandmark = handLandmarks.landmark[point]
-                pixelCoord = drawingModule._normalized_to_pixel_coordinates(normalizedLandmark.x,
-                                                                                          normalizedLandmark.y,
-                                                                                          imageWidth, imageHeight)
+        imageHeight, imageWidth, _ = image.shape
 
-                Ps, Pe = pixelCoord
-                Ps1 = Ps - 20
+        if results.multi_hand_landmarks != None:
+            for handLandmarks in results.multi_hand_landmarks:
+                drawingModule.draw_landmarks(image, handLandmarks, handsModule.HAND_CONNECTIONS)
+                for point in handsModule.HandLandmark:
+                    normalizedLandmark = handLandmarks.landmark[point]
+                    pixelCoord = drawingModule._normalized_to_pixel_coordinates(normalizedLandmark.x,
+                                                                                              normalizedLandmark.y,
+                                                                                              imageWidth, imageHeight)
 
-                Pe1 = Ps1 + 40
+                    Ps, Pe = pixelCoord
+                    Ps1 = Ps - 20
 
-                startPoint = (Ps1, Pe)
-                endPoint = (Pe1, Pe)
+                    Pe1 = Ps1 + 40
 
-                image = cv2.line(image, startPoint, endPoint, color=(0,255,0), thickness=2)
+                    startPoint = (Ps1, Pe)
+                    endPoint = (Pe1, Pe)
 
-    cv2.imshow('Hand', image)
+                    image = cv2.line(image, startPoint, endPoint, color=(0,255,0), thickness=2)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        cv2.imshow('Hand', image)
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+img1 = r'C:\Users\Cullen\Downloads\IMG_2555.jpg'
+img2 = r'C:\Users\Cullen\Downloads\IMG_2560.jpg'
+img3 = r'C:\Users\Cullen\Downloads\IMG_2559.jpg'
+img4 = r'C:\Users\Cullen\Downloads\IMG_2558.jpg'
+img5 = r'C:\Users\Cullen\Downloads\IMG_2557.jpg'
+
+HandFeatures(img1)
+HandFeatures(img2)
+HandFeatures(img3)
+HandFeatures(img4)
+HandFeatures(img5)
